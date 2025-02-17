64bad7f1e353c772b07ea64daa0283a9d07bd87c
Benji Shults <14838863+benjishults@users.noreply.github.com>
2025-02-17 15:01:00 -0600

    edits to sample app

--- composeApp/src/desktopMain/kotlin/bps/budget/main.kt
+++ composeApp/src/desktopMain/kotlin/bps/budget/main.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.window.application
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "budget-kmp",
+        title = "BPS Budget",
     ) {
         App()
     }
