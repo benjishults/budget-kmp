ec90201ecc13072110aab180fdc2ad282c5d44c0
Benji Shults <14838863+benjishults@users.noreply.github.com>
2025-07-19 15:13:45 -0500

    order real accounts first

--- cli/scripts/budget.sh
+++ cli/scripts/budget.sh
@@ -1,5 +1,13 @@
 if [ "$1" = 'debug' ]; then
-  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=1044 -cp ../build/libs/cli-1.0-SNAPSHOT-all.jar -DBPS_BUDGET_LOG_FOLDER=~/.local/share/bps-budget/logs bps.budget.Budget
+  java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=1044 \
+      -cp "${BPS_BUDGET_CLI_DEBUG_CLASSPATH:-./cli/build/libs}/cli-1.0-SNAPSHOT-all.jar" \
+      bps.budget.Budget
+elif [ "$1" = 'test' ]; then
+  java \
+      -cp "${BPS_BUDGET_CLI_DEBUG_CLASSPATH:-./cli/build/libs}/cli-1.0-SNAPSHOT-all.jar" \
+      bps.budget.Budget
 else
-  java -cp ../build/libs/cli-1.0-SNAPSHOT-all.jar -DBPS_BUDGET_LOG_FOLDER=~/.local/share/bps-budget/logs bps.budget.Budget
+  java \
+      -cp "${BPS_BUDGET_CLI_CLASSPATH:-${HOME}/.local/share/bps-budget/libs}/cli-1.0-SNAPSHOT-all.jar" \
+      bps.budget.Budget
 fi
